22627d4b5f7e33e8fc943f62f13104a3a0355a50
Melike Goren <melike.goren@outlook.com>
2022-12-07 21:16:30 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import sqlite3 as sql
+connect = sql.connect('deneme.db')
+print('deneme dosyasi olusturuldu')
+cursor = connect.cursor()
+print('cursor olusturuldu')
+
+cursor.execute(""" CREATE TABLE IF NOT EXISTS STUDENTS(
+    name text,
+    lastname text,
+    age integer)
+""")
+cursor.execute(""" INSERT INTO STUDENTS VALUES('Melike', 'Gören', 22)
+""")
+
+connect.commit()
+connect.close()
